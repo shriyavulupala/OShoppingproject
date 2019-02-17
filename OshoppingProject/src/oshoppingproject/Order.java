12f44c9211e50af77cde4828b33b4e48c88165ae
1895730 <1895730@E21033.CEGEPGIM.LCL>
2019-02-17 14:09:35 -0500

    Order with getter, setter and constructor methods

--- OshoppingProject/src/oshoppingproject/Order.java
+++ OshoppingProject/src/oshoppingproject/Order.java
@@ -5,6 +5,8 @@
  */
 package oshoppingproject;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -12,7 +14,49 @@ import java.util.Date;
  * @author 1895730
  */
 public class Order {
-    Date date;
     String paymethod;
     float amount;
+    Date date = parseDate("2019-02-17");
+    public Order(String paymethod,float amount)
+    {
+        this.paymethod=paymethod;
+        this.amount=amount;
+    }
+    public void setDate(Date d)
+    {
+        this.date=d;
+    }
+    public Date getDate()
+    {
+        return date;
+    }
+    public void setpaymethod(String p)
+    {
+        this.paymethod=p;
+    }
+    public String getpaymethod()
+    {
+        return paymethod;
+    }
+    public void setamount(float a)
+    {
+        this.amount=a;
+    }
+    public float getamount()
+    {
+        return amount;
+    }
+      public void confirm()
+    {
+        System.out.println("Date is: "+date+"\npaydetails"+paymethod);
+    }
+
+     public static Date parseDate(String date) {
+     try {
+         return new SimpleDateFormat("yyyy-MM-dd").parse(date);
+     } 
+     catch (ParseException e) {
+         return null;
+     }
+     }
 }
