282e2c6ab52bfd647381adaa8974aa2367dc0d91
1895730 <1895730@E21033.CEGEPGIM.LCL>
2019-02-17 14:37:22 -0500

    commiting main method

--- OshoppingProject/src/oshoppingproject/Brand.java
+++ OshoppingProject/src/oshoppingproject/Brand.java
@@ -13,11 +13,44 @@ public class Brand {
     String name;
     String id;
     String description;
-    public void setname(String name, String id,String description)
+    /*
+    public Brand(String name, String id,String description)
     {
       this.name = name;
       this.id=id;
       this.description=description;
+    }*/
+    
+    public void setName(String n)
+    {
+        this.name=n;
+    }
+    public String getName()
+    {
+        return name;
     }
     
+    public void setid(String id)
+    {
+        this.id=id;
+    }
+    
+    public String getid()
+    {
+        return id;
+    }
+    
+      public void setDescription(String d)
+    {
+        this.description=d;
+    }
+    public String getDescription()
+    {
+        return description;
+    }
+    
+    public void display()
+    {
+    System.out.println("Product Name: "+name+"\nProduct ID: "+id+"\nProduct Description: "+description);
+    }
 }
