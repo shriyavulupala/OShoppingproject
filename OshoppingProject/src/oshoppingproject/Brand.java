17934640303450e87437542b7d1565b76396e438
1893716 <1893716@E21032.CEGEPGIM.LCL>
2019-02-17 11:36:35 -0500

    Commiting Brand class

---
+++ OshoppingProject/src/oshoppingproject/Brand.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oshoppingproject;
+
+/**
+ *
+ * @author 1893716
+ */
+public class Brand {
+    String name;
+    String id;
+    String description;
+    
+}
