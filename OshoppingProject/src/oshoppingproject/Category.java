c8652a3ebbebaaaa2f11e3d71c745ec69a861176
1895198 <1895198@E21056.CEGEPGIM.LCL>
2019-03-02 14:04:49 -0500

    Commiting Category with new String ID attribute - Shriya

--- OshoppingProject/src/oshoppingproject/Category.java
+++ OshoppingProject/src/oshoppingproject/Category.java
@@ -12,8 +12,16 @@ package oshoppingproject;
 public class Category
 {
     String name,description;
+    private String ID;
     int id;
     
+    public Category(int id, String name, String description ,String ID)
+    {
+        this.id=id;
+        this.name=name;
+        this.description=description;
+        this.ID=ID;
+    }
     public void setName(String n)
     {
         this.name=n;
@@ -32,17 +40,28 @@ public class Category
         return description;
     }
     
-    public void setId(int ID)
+    public void setId(int id)
     {
-        this.id=ID;
+        this.id=id;
     }
-    public int getID()
+    public int getid()
     {
      return id;   
     }
      public void display()
     {
     System.out.println("Name: "+name+"\n ID: "+id+"\n Description: "+description);
+    }
+
+    /**
+     * @param ID the ID to set
+     */
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+    public String getID()
+    {
+     return ID;   
     }
     
     
