2587fb7f7a11abeaf5c7a70bf89486bc08c4e2d4
1893716 <1893716@E21051.CEGEPGIM.LCL>
2019-03-02 13:03:03 -0500

    creating id for detail class

--- OshoppingProject/src/oshoppingproject/detail.java
+++ OshoppingProject/src/oshoppingproject/detail.java
@@ -11,17 +11,46 @@ package oshoppingproject;
  * @author 1893716
  */
 public class detail {
-    String quantity;
+    private String quantity;
+    private String ID;
     public void setquantity(String quantity )
     {
-        this.quantity=quantity;
+        this.setQuantity(quantity);
     }
     public String getquantity(){
-        return quantity;
+        return getQuantity();
         
     }
              
     public void display(){
-        System.out.println("quantity detail: "+ quantity);
+        System.out.println("quantity detail: "+ getQuantity());
+    }
+
+    /**
+     * @return the quantity
+     */
+    public String getQuantity() {
+        return quantity;
+    }
+
+    /**
+     * @param quantity the quantity to set
+     */
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    /**
+     * @return the ID
+     */
+    public String getID() {
+        return ID;
+    }
+
+    /**
+     * @param ID the ID to set
+     */
+    public void setID(String ID) {
+        this.ID = ID;
     }
 }
