1304a0e917eb94dec03a112632e64490c7c109a0
1895730 <1895730@E21033.CEGEPGIM.LCL>
2019-03-02 15:04:50 -0500

    final commit

--- OshoppingProject/src/oshoppingproject/Category.java
+++ OshoppingProject/src/oshoppingproject/Category.java
@@ -12,15 +12,15 @@ package oshoppingproject;
 public class Category
 {
     String name,description;
-    private String ID;
+   
     int id;
     
-    public Category(int id, String name, String description ,String ID)
+    public Category(int id, String name, String description)
     {
         this.id=id;
         this.name=name;
         this.description=description;
-        this.ID=ID;
+     
     }
     public void setName(String n)
     {
@@ -52,17 +52,4 @@ public class Category
     {
     System.out.println("Name: "+name+"\n ID: "+id+"\n Description: "+description);
     }
-
-    /**
-     * @param ID the ID to set
-     */
-    public void setID(String ID) {
-        this.ID = ID;
-    }
-    public String getID()
-    {
-     return ID;   
-    }
-    
-    
 }
