e6bf4d6ed45a8499fc0fa1bcf4b47d131cfeae22
1895730 <1895730@E21033.CEGEPGIM.LCL>
2019-03-02 16:17:28 -0500

    testing customer class

---
+++ OshoppingProject/test/CustomerTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author 1895730
+ */
+public class CustomerTest {
+    
+    public CustomerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+   public void Customertest()
+   {
+       int custid= 45;
+         String paymethod="credit card";
+         String Id="C";
+         String expected =custid+" "+paymethod+" "+Id;
+         String real=oshoppingproject.Customer.Customer(custid, paymethod, Id);
+         assertEquals(expected,real);
+   }
+}
