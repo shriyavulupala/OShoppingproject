ca225c5cfaf052dd95ae90700a899785bfe6e1ad
1895730 <1895730@E21033.CEGEPGIM.LCL>
2019-03-02 15:48:45 -0500

    testing order class

---
+++ OshoppingProject/test/OrderTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import oshoppingproject.Order;
+
+/**
+ *
+ * @author 1895730
+ */
+public class OrderTest {
+    
+    public OrderTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+     @Test
+     public void Ordertest(){
+         String name="sfdadsf";
+         float num=20;
+         String Id="189956";
+         String expected =num+" "+name+" "+Id;
+         String real=Order.Order(num,name,Id);
+         assertEquals(expected,real);
+    // public void hello() {}
+}
+}
