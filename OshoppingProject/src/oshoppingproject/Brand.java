f32dda2ebb5d89a34e1bddb6768e079cc5ddf9e6
1893716 <1893716@E21032.CEGEPGIM.LCL>
2019-03-03 10:01:11 -0500

    commiting detail class

--- OshoppingProject/src/oshoppingproject/Brand.java
+++ OshoppingProject/src/oshoppingproject/Brand.java
@@ -13,33 +13,72 @@ import java.util.Date;
  * @author 1893716
  */
 public class Brand {
-     String name;
-    String id;
-    String description;
+     private String name;
+    private String id;
+    private String description;
     
-    /*
+   
     public Brand(String name, String id,String description)
     {
       this.name = name;
       this.id=id;
       this.description=description;
-    }*/
+    }
     
-    public void setName(String n)
-    {
-        this.name=n;
-    }
-    public String getName()
-    {
-        return name;
-    }
-
+  
+ 
 public static String brandtest(String name,String date, String description , String ID)
 {
     return name+" "+date+" "+description+" "+ID;
 }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void display()
+    {
+    System.out.println("name: "+id+" "+description);
+    }
     /**
      * @return the ID
      */
-    
+   
 }
